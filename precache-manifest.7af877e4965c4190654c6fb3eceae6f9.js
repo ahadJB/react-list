bf5a37ff6350a128a038214706a04f2472855216
ahadJB <wwfbcom@gmail.com>
2019-03-16 17:19:57 +0600

    add react app

---
+++ precache-manifest.7af877e4965c4190654c6fb3eceae6f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d9d1b5293e72786dd966",
+    "url": "/static/js/main.17d7b7bc.chunk.js"
+  },
+  {
+    "revision": "4e06cbc20a459c856eea",
+    "url": "/static/js/2.e0b6258c.chunk.js"
+  },
+  {
+    "revision": "d9d1b5293e72786dd966",
+    "url": "/static/css/main.b730ea94.chunk.css"
+  },
+  {
+    "revision": "94dc1eca323087ea29c2d4358c070ee0",
+    "url": "/index.html"
+  }
+];
